78715365fce0190f57f3c16b02138a7a86a593ee
BlackPrick <122222996+BlackPrick@users.noreply.github.com>
2023-02-12 01:48:10 +0000

    Program upload

---
+++ script.js
@@ -0,0 +1,195 @@
+const BUTTONS = document.querySelectorAll('button')
+const RESULT_INP = document.querySelector('.action-inp')
+const HISTORY_INP = document.querySelector('.history-inp')
+let pressedBtn;
+
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+let isCalculated = false;
+
+// Events after button click
+BUTTONS.forEach((btn) => {
+    const dataType = btn.getAttribute('data-type')
+    btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        setAction(dataType, btn)
+    })
+
+    btn.addEventListener("mousedown", () => {
+        btn.classList.add('pressed')
+        pressedBtn = btn
+    })
+})
+
+// Events after keydown
+window.addEventListener('keydown', function (e) {
+    if (document.querySelector(`button[value="${e.key}"]`) !== null) {
+        const btn = document.querySelector(`button[value="${e.key}"]`)
+        const dataType = btn.getAttribute('data-type')
+        setAction(dataType, btn)
+        btn.classList.add('pressed')
+        pressedBtn = btn
+    }
+})
+
+// Set action after interaction with buttons
+function setAction(dataType, btn) {
+    switch (dataType) {
+        case "number":
+            operandConstructor(btn);
+            break;
+        case "operation":
+            setOperator(btn)
+            break;
+        case "result":
+            calculation(btn)
+            break;
+        case "delete":
+            deleteSymbol()
+            break;
+        case "clean":
+            cleanCalculator()
+            break;
+        case "negate":
+            negateNumber()
+            break;
+    }
+}
+
+// Set or get current operand
+function manageOperand(action, value) {
+    switch (action) {
+        case 'get':
+            return (operator === "") ? operand1 : operand2;
+        case 'set':
+            if (operator === "") operand1 = value
+            else operand2 = value
+    }
+}
+
+// Creating operands
+function operandConstructor(btn) {
+    if (isCalculated) cleanCalculator()
+    let key = btn.innerText
+    let currentOperand = manageOperand('get')
+    
+    if (currentOperand.length == 13) return;
+    else if (key === "." && currentOperand.includes(".")) return;
+    else if (key === "0" && currentOperand==="0") return;
+    else if (key === "." && currentOperand === "") currentOperand = '0.';
+    else if (key !== "." && key !== "0" && currentOperand === "0") currentOperand = key;
+    else currentOperand += key
+
+    RESULT_INP.value = currentOperand;
+    manageOperand('set', currentOperand)
+}
+
+// Set operator for calculation
+function setOperator(btn) {
+    operator = btn.getAttribute("value")
+    HISTORY_INP.value = (operand1 !== "") ? ((+operand1) + " " + operator) : "0 " + operator;
+    RESULT_INP.value = (operand1 !== "") ? (+operand1) : "0";
+    isCalculated = false;
+    operand2 = "";
+}
+
+// Indicate calculation type and get result
+function calculation(btn) {
+    if (operator === "") return;
+    if (operand2 === "") operand2 = operand1;
+    let key = btn.innerText.trim()
+    let a = Number(operand1);
+    let b = Number(operand2);
+
+    if (key === "=") {
+        switch (operator) {
+            case '+':
+                operand1 = a + b;
+                break;
+            case '-':
+                operand1 = a - b;
+                break;
+            case '*':
+                operand1 = a * b;
+                break;
+            case '/':
+                if (b !== 0) operand1 = a / b
+                else {
+                    cleanCalculator()
+                    RESULT_INP.value = 'Error';
+                    return;
+                }
+                break;
+        }
+    }
+    else if (key === "%") {
+        let index = (operator === "-" || operator === "+") ? 100 : 1000;
+        operand1 = a / index * b
+    }
+
+    operand1 = lengthControl(operand1)
+
+    HISTORY_INP.value = a + " " + operator + " " + b + " =";
+    RESULT_INP.value = +operand1
+
+    isCalculated = true
+    operand1 = operand1.toString()
+}
+
+// Truncate infinity/floats and big numbers to exponential
+function lengthControl(number) {
+    // Remove inaccuracy
+    number = Number(number.toFixed(13))
+    if (operand1.toString().length <= 13) return number;
+
+    if (number % 1 !== 0) return Math.round((number + Number.EPSILON) * 100) / 100;
+    else return number.toExponential(6)
+}
+
+// Clean calculator
+function cleanCalculator() {
+    operand1 = "";
+    operand2 = "";
+    operator = "";
+    HISTORY_INP.value = "";
+    RESULT_INP.value = "0";
+    isCalculated = false;
+}
+
+// Backspace function 
+function deleteSymbol() {
+    if (isCalculated === true) HISTORY_INP.value = "";
+    else if (operator !== "") {
+        operand2 = (operand2.length > 1) ? operand2.slice(0, -1) : "";
+        RESULT_INP.value = (operand2 !== "") ? Number(operand2) : "0"
+    }
+    else {
+        operand1 = (operand1.length > 1) ? operand1.slice(0, -1) : "";
+        RESULT_INP.value = (operand1 !== "") ? Number(operand1) : "0"
+    }
+}
+
+// Negate number
+function negateNumber() {
+    let currentOperand = manageOperand('get')
+    if(currentOperand==="" || currentOperand==="0") return;
+    currentOperand = +currentOperand * -1;
+    RESULT_INP.value = currentOperand
+    currentOperand = currentOperand.toString()
+    manageOperand('set', currentOperand)
+}
+
+// Remove pressed button style
+document.addEventListener('mouseup', () => {
+    if (pressedBtn !== undefined) {
+        pressedBtn.classList.remove("pressed")
+        pressedBtn = undefined;
+    }
+})
+document.addEventListener('keyup', () => {
+    if (pressedBtn !== undefined) {
+        pressedBtn.classList.remove("pressed")
+        pressedBtn = undefined;
+    }
+})
